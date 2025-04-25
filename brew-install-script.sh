e6df02afa0eca6eb0cce90cd295893e1967f47ad
Hatlord <emailrich@gmail.com>
2025-04-25 09:13:28 +0100

    Update brew-install-script.sh

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -27,8 +27,8 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 echo "Installing brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo >> /Users/rich/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/rich/.zprofile
+echo >> ~/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 #update brew
